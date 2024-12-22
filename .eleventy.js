bf6132a45c99e65e266625d021c6aaa3bfdc786e
Trang Dông <trangdong@trangs-mbp.speedport.ip>
2024-12-22 23:11:31 +0100

    Add .nojekyll to bypass Jekyll

--- .eleventy.js
+++ .eleventy.js
@@ -19,7 +19,7 @@ module.exports = (config) => {
         htmlTemplateEngine: 'njk',
         dir: {
             input: 'src',
-            output: 'dist',
+            output: 'docs',
         },
     };
 };
